e746a3478c3c6836a91ee459bcdeb3c6bade32b6
Gustavo <g.rezendesilva@tudelft.nl>
2022-05-11 17:50:07 +0200

    🔧 add resource folder and export it in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
+package_name ='mavros_wrapper'
+
 d = generate_distutils_setup(
     packages=['mavros_wrapper'],
     package_dir={'': 'src'},
+    data_files=[
+        ('share/' + package_name, ['package.xml']),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+    ]
 )
 
 setup(**d)
